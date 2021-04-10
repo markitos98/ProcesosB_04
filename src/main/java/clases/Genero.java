80e2324f0c69b090eff3382187ff6252ee61e195
leiregago <leire.gago@opendeusto.es>
2021-04-10 10:06:01 +0200

    enum genero

---
+++ src/main/java/clases/Genero.java
@@ -0,0 +1,10 @@
+package clases;
+
+public enum Genero {
+
+	TERROR,ACCION,COMEDIA,ROMANTICA,THRILLER,SCIFI,HISTORIA,CLASICA,MUSICAL,DRAMA,INFANTIL;
+
+    static Genero getGenero(String elemento){
+        return Enum.valueOf(Genero.class,elemento);}
+	
+}
