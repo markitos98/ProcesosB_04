e4a91ef5b381a6d17a029ddfe1759311fa36493e
leiregago <leire.gago@opendeusto.es>
2021-04-17 12:00:12 +0200

    Sesion

--- src/test/java/B_04/maven/eclipse/SesionTest.java
+++ src/test/java/B_04/maven/eclipse/SesionTest.java
@@ -1,39 +1,39 @@
 package B_04.maven.eclipse;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import clases.Sesion;
 
-class SesionTest {
+public class SesionTest {
 
-	 private Sesion s;
-	    private Sesion prueba;
-	    private Sesion empty;
+	private Sesion s;
+    private Sesion prueba;
+    private Sesion empty;
 
-	    @Before
-	    public void setUp() {
+    @Before
+    public void setUp() {
 
-	        s=  new Sesion(22.00);
-	        prueba=  new Sesion(s);
-	        empty= new Sesion();
+        s=  new Sesion(22.00);
+        prueba=  new Sesion(s);
+        empty= new Sesion();
 
-	    }
-	    @Test
+    }
+    @Test
 
-	    public void testGetHora() {
-	        assertEquals(22.00, s.getHora(),20.00);
-	        assertEquals(20.00, prueba.getHora(),20.00);
-	        assertEquals(18.00, empty.getHora(),20.00);
-	    }
+    public void testGetHora() {
+        assertEquals(22.00, s.getHora(),20.00);
+        assertEquals(20.00, prueba.getHora(),20.00);
+        assertEquals(18.00, empty.getHora(),20.00);
+    }
 
-	    @Test
-	    public void testSetHora() {
-	        s.setHora(18.00);
-	        assertEquals(18.00, s.getHora(),20.00);
+    @Test
+    public void testSetHora() {
+        s.setHora(18.00);
+        assertEquals(18.00, s.getHora(),20.00);
 
-	    }
+    }
 
 }
