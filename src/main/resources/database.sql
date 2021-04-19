0b9b1c283a29ca0bb2aae36fb54fedfb3ab9c18b
markitos98 <marcos.llorente@opendeusto.es>
2021-04-19 19:33:44 +0200

    Prueba de base de datos hecha

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,11 +1,11 @@
 /* DELETE 'nucleus' database*/
-DROP SCHEMA IF EXISTS cinebd;
+DROP SCHEMA IF EXISTS cineDB;
 /* DELETE USER 'spq' AT LOCAL SERVER*/
 DROP USER IF EXISTS 'spq'@'%';
 
 /* CREATE 'nucleus' DATABASE */
-CREATE SCHEMA cinebd;
+CREATE SCHEMA cineDB;
 /* CREATE THE USER 'spq' AT LOCAL SERVER WITH PASSWORD 'spq' */
 CREATE USER IF NOT EXISTS 'spq'@'%' IDENTIFIED BY 'spq';
 
-GRANT ALL ON cinebd.* TO 'spq'@'%';
+GRANT ALL ON cineDB.* TO 'spq'@'%';
