9c3e119640231f9c5c357b2ec7d68faf21b0a474
gorkazarate <52033110+gorkazarate@users.noreply.github.com>
2021-04-29 17:52:19 +0200

    Databasetest

--- src/test/java/B_04/maven/eclipse/DatabaseTest.java
+++ src/test/java/B_04/maven/eclipse/DatabaseTest.java
@@ -1,16 +1,30 @@
 package B_04.maven.eclipse;
 
+import static org.junit.Assert.assertArrayEquals;
+
+import javax.jdo.PersistenceManager;
+
 import org.mockito.Mock;
 
+import clasesUsuario.Usuario;
 import database.Database;
+import clasesUsuario.Cliente;
 
 public class DatabaseTest {
 	
 	@Mock
 	
 	Database databaseMock;
+	private Usuario u;
+	private Cliente cl;
 	
 	
-	
-
+	public void getUsuarioTest() {
+		 u= new Usuario() {
+		};
+		cl= new Cliente();
+		
+		
+		
+}
 }
