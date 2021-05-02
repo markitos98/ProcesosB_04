826359237858b16096e7e83bea349713876a292a
alex <alex.anton@opendeusto.es>
2021-05-02 19:37:18 +0200

    Modificacion atributos clase Entrada

--- src/main/java/clases/Entrada.java
+++ src/main/java/clases/Entrada.java
@@ -18,16 +18,21 @@ public class Entrada{
 	private Sesion infoSesion;
 	private int precio;
 	private int cantidad;
+	private int precioTotal;
+	private String nombre;
 
 
 
-	public Entrada(int id, Pelicula infoPelicula, Sesion infoSesion, int precio, int cantidad) {
+	public Entrada(int id, Pelicula infoPelicula, Sesion infoSesion, int precio, int cantidad, int precioTotal, String nombre) {
 		super();
 		this.setId((int)(Math.random()*10000+1));
 		this.infoPelicula = infoPelicula;
 		this.infoSesion = infoSesion;
 		this.precio = precio;
 		this.cantidad = cantidad;
+		this.precio = precioTotal;
+		this.nombre = nombre;
+		
 	}
 	public Entrada(Entrada e) {
 		super();
@@ -35,6 +40,8 @@ public class Entrada{
 		this.infoSesion = e.infoSesion;
 		this.precio = e.precio;
 		this.cantidad = e.cantidad;
+		this.precioTotal = e.precioTotal;
+		this.nombre = e.nombre;
 	}
 
 	public Entrada() {
@@ -65,6 +72,18 @@ public class Entrada{
 		this.precio = precio;
 	}
 
+	public int getPrecioTotal() {
+		return precioTotal;
+	}
+	public void setPrecioTotal(int precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 	public int getCantidad() {
 		return cantidad;
 	}
@@ -79,6 +98,9 @@ public class Entrada{
 		System.out.println("Sesion:" + "<" + infoSesion.getHora() + ">");
 		System.out.println("Cantidad:" + "<" + cantidad + ">");
 		System.out.println("Precio:" + "<" + precio + ">");
+		System.out.println("PrecioTotal:" + "<" + precioTotal + ">");
+		System.out.println("Nombre:" + "<" + nombre + ">");
+		
 
 	}
 	public int getId() {
