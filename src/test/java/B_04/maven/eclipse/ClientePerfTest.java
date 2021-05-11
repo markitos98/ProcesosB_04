f0a8bd4e214a3fd47cfb267197fd0168b2504d27
alex <alex.anton@opendeusto.es>
2021-05-11 22:56:17 +0200

    Test ClientePerfTest hecho

---
+++ src/test/java/B_04/maven/eclipse/ClientePerfTest.java
@@ -0,0 +1,79 @@
+package B_04.maven.eclipse;
+
+import org.apache.log4j.Logger;
+
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.Required;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import clases.DAO.UsuarioDAO;
+import clasesUsuario.Cliente;
+import junit.framework.JUnit4TestAdapter;
+
+
+
+public class ClientePerfTest {
+
+	private Cliente c= new Cliente("alex","1234","alex@gmail.com","Alex","Anton","Mota","27/09/1999");
+;
+	
+	
+	
+	static Logger logger = Logger.getLogger(ClientePerfTest.class.getName());
+
+	// If you use the EmptyReportModule, the report is not generated
+	//@Rule public ContiPerfRule rule = new ContiPerfRule(new EmptyReportModule());
+	
+	public static junit.framework.Test suite() {
+		 return new JUnit4TestAdapter(ClientePerfTest.class);
+	}
+	
+	@Rule
+    public ContiPerfRule rule = new ContiPerfRule();
+	
+	@Test
+	@PerfTest(invocations = 120)
+	public void testCrearCliente() throws Exception {
+	    Cliente cliente=new Cliente();
+	   cliente.setUsername("username");
+	   cliente.setPassword("password");
+	   cliente.setEmail("email");
+	   cliente.setNombre("nombre");
+	   cliente.setApellido_1("apellido1");
+	   cliente.setApellido_2("apellido2");
+	   cliente.setFecha_nac("fechanac");
+	}
+	@Test
+	@PerfTest(invocations = 120)
+	public void testGetClientes() throws Exception {
+	   UsuarioDAO udao= new UsuarioDAO();
+	   udao.getUsuario(c.getUsername());
+	   
+	}
+	@Test
+	@PerfTest(invocations = 120)
+	public void testComprobarClientes() throws Exception {
+	   UsuarioDAO udao= new UsuarioDAO();
+	   udao.comprobarUsuario(c.getUsername(),c.getPassword());
+	   
+	}
+	@Test
+	@PerfTest(invocations = 120)
+	public void testComprobarEmail() throws Exception {
+	   UsuarioDAO udao= new UsuarioDAO();
+	   udao.comprobarEmail(c.getEmail());
+	   
+	}
+	@Test
+	@PerfTest(invocations = 120)
+	public void testDeleteUsuario() throws Exception {
+	   UsuarioDAO udao= new UsuarioDAO();
+	   udao.deleteUsuario(c);
+	   
+	}
+	
+	
+}
