300fc33c86bc6187d1f0a5957ca32039c6cc01eb
alex <alex.anton@opendeusto.es>
2021-05-20 15:09:36 +0200

    Caratulas añadidas y datos añadidos

--- src/main/java/database/DatosPrueba.java
+++ src/main/java/database/DatosPrueba.java
@@ -34,36 +34,36 @@ public class DatosPrueba {
 			
 			
 			
-			Pelicula p3=  new Pelicula("Los Vengadores","ACCION",2019,"Después de los eventos devastadores de 'Avengers: Infinity War', , el Titán Loco. Con la ay",220,"https://youtu.be/1XrrTJpA3yM","/resources/imagenes/Malasaña 32.jpg","posters/Malasaña.jpg",2);
+			Pelicula p3=  new Pelicula("Los Vengadores","ACCION",2019,"Película de marvel",220,"https://youtu.be/1XrrTJpA3yM","/resources/imagenes/Malasaña 32.jpg","posters/Malasaña.jpg",2);
 			pm.makePersistent(p3);
 			
 			
 			
 		
 			
-			Pelicula p4=  new Pelicula(" 32","TERROR",2020,"Manolo y Candela se instalan en el madrileño barrio de Malasaña, junto a sus tres hijos y el abuelo Ferm",120,"https://www.youtube.com/watch?v=ampK3e410J4&ab_channel=https://www.youtube.com/watch?v=TdhAMWX0ok4&ab_channel=VaDeTrailers","/resources/imagenes/Malasaña 32.jpg","posters/Malasaña.jpg",2);
+			Pelicula p4=  new Pelicula("Annabelle vuelve a casa","TERROR",2020,"Película de la muñeca de terror",120,"https://www.youtube.com/watch?v=1VTnFrWGD-w&ab_channel=WarnerBros.PicturesEspa%C3%B1a","/resources/imagenes/Annabelle vuelve a casa.jpg","posters/Malasaña.jpg",2);
 			pm.makePersistent(p4);
 			
 			
-			Pelicula p5=  new Pelicula(" 32q","TERROR",2020,"Manolo y Candela se instalan en el madrileño barrio de Malasaña, junto a sus tres hijos.",120,"https://www.youtube.com/watch?v=ampK3e410J4&ab_channel=https://www.youtube.com/watch?v=TdhAMWX0ok4&ab_channel=VaDeTrailers","/resources/imagenes/Malasaña 32.jpg","posters/Malasaña.jpg",2);
+			Pelicula p5=  new Pelicula("Escuadron suicida","ACCION",2020,"Película de marvel",120,"https://www.youtube.com/watch?v=ampK3e410J4&ab_channel=https://www.youtube.com/watch?v=TdhAMWX0ok4&ab_channel=VaDeTrailers","/resources/imagenes/Escuadron suicida.jpeg","posters/Malasaña.jpg",2);
 			pm.makePersistent(p5);
 			
-			Pelicula p6=  new Pelicula("j 32","TERROR",2020,"Manolo y Candela se instalan en el madrileño barrio de Malasaña, junto a sus tres hijos",120,"https://www.youtube.com/watch?v=ampK3e410J4&ab_channel=https://www.youtube.com/watch?v=TdhAMWX0ok4&ab_channel=VaDeTrailers","/resources/imagenes/Malasaña 32.jpg","posters/Malasaña.jpg",2);
+			Pelicula p6=  new Pelicula("Mortal Kombat","ACCION",2020,"Manolo y Candela se instalan en el madrileño barrio de Malasaña, junto a sus tres hijos",120,"https://www.youtube.com/watch?v=ampK3e410J4&ab_channel=https://www.youtube.com/watch?v=TdhAMWX0ok4&ab_channel=VaDeTrailers","/resources/imagenes/Mortal kombat.jpg","posters/Malasaña.jpg",2);
 			pm.makePersistent(p6);
 			
-			Pelicula p7=  new Pelicula("k 32","TERROR",2020,"Manolo y Candela se instalan en el madrileño barrio de Malasaña, junto a sus tres hijos y el abuelo Fe.",120,"https://www.youtube.com/watch?v=ampK3e410J4&ab_channel=https://www.youtube.com/watch?v=TdhAMWX0ok4&ab_channel=VaDeTrailers","/resources/imagenes/Malasaña 32.jpg","posters/Malasaña.jpg",2);
+			Pelicula p7=  new Pelicula("Soul","INFANTIL",2020,"Manolo y Candela se instalan en el madrileño barrio de Malasaña, junto a sus tres hijos y el abuelo Fe.",120,"https://www.youtube.com/watch?v=ampK3e410J4&ab_channel=https://www.youtube.com/watch?v=TdhAMWX0ok4&ab_channel=VaDeTrailers","/resources/imagenes/Soul.jpeg","posters/Malasaña.jpg",2);
 			pm.makePersistent(p7);
 			
-			Pelicula p8=  new Pelicula("u 32","TERROR",2020,"Manolo y Candela se instalan en el madrileño barrio de ",120,"https://www.youtube.com/watch?v=ampK3e410J4&ab_channel=https://www.youtube.com/watch?v=TdhAMWX0ok4&ab_channel=VaDeTrailers","/resources/imagenes/Malasaña 32.jpg","posters/Malasaña.jpg",2);
+			Pelicula p8=  new Pelicula("Mulan","INFANTIL",2020,"Manolo y Candela se instalan en el madrileño barrio de ",120,"https://www.youtube.com/watch?v=ampK3e410J4&ab_channel=https://www.youtube.com/watch?v=TdhAMWX0ok4&ab_channel=VaDeTrailers","/resources/imagenes/Mulan.jpeg","posters/Malasaña.jpg",2);
 			pm.makePersistent(p8);
 			
-			Pelicula p9=  new Pelicula("o 32","TERROR",2020,"Manolo y Candela se instalan en el madrileño barrio de Malasaña, ju.",120,"https://www.youtube.com/watch?v=ampK3e410J4&ab_channel=https://www.youtube.com/watch?v=TdhAMWX0ok4&ab_channel=VaDeTrailers","/resources/imagenes/Malasaña 32.jpg","posters/Malasaña.jpg",2);
+			Pelicula p9=  new Pelicula("Oxygen","SUSPENSE",2020,"Manolo y Candela se instalan en el madrileño barrio de Malasaña, ju.",120,"https://www.youtube.com/watch?v=ampK3e410J4&ab_channel=https://www.youtube.com/watch?v=TdhAMWX0ok4&ab_channel=VaDeTrailers","/resources/imagenes/Oxygen.jpeg","posters/Malasaña.jpg",2);
 			pm.makePersistent(p9);
 			
-			Pelicula p10=  new Pelicula("q 32","TERROR",2020,"Manolo y Candela se instalan en el madrileño barrio de Malasaña, jun",120,"https://www.youtube.com/watch?v=ampK3e410J4&ab_channel=https://www.youtube.com/watch?v=TdhAMWX0ok4&ab_channel=VaDeTrailers","/resources/imagenes/Malasaña 32.jpg","posters/Malasaña.jpg",2);
+			Pelicula p10=  new Pelicula("Stowaway","ACCION",2020,"Manolo y Candela se instalan en el madrileño barrio de Malasaña, jun",120,"https://www.youtube.com/watch?v=ampK3e410J4&ab_channel=https://www.youtube.com/watch?v=TdhAMWX0ok4&ab_channel=VaDeTrailers","/resources/imagenes/Stowaway.jpeg","posters/Malasaña.jpg",2);
 			pm.makePersistent(p10);
 			
-			Pelicula p11=  new Pelicula("w 32","TERROR",2020,"Manolo y Candela se instalan en el madrileñ",120,"https://www.youtube.com/watch?v=ampK3e410J4&ab_channel=https://www.youtube.com/watch?v=TdhAMWX0ok4&ab_channel=VaDeTrailers","/resources/imagenes/Malasaña 32.jpg","posters/Malasaña.jpg",2);
+			Pelicula p11=  new Pelicula("Twist","TERROR",2020,"Manolo y Candela se instalan en el madrileñ",120,"https://www.youtube.com/watch?v=ampK3e410J4&ab_channel=https://www.youtube.com/watch?v=TdhAMWX0ok4&ab_channel=VaDeTrailers","/resources/imagenes/Twist.jpg","posters/Malasaña.jpg",2);
 			pm.makePersistent(p11);
 			tx.commit();
 		} finally {
