a51281fbf54761251d554b10bf5e18d3d97b612a
gorkazarate <52033110+gorkazarate@users.noreply.github.com>
2021-05-22 11:54:00 +0200

    comprobar usuario

--- src/test/java/B_04/maven/eclipse/ComprobarUsuarioPerfTest.java
+++ src/test/java/B_04/maven/eclipse/ComprobarUsuarioPerfTest.java
@@ -13,20 +13,20 @@ import clasesUsuario.Cliente;
 import junit.framework.JUnit4TestAdapter;
 
 @Category(IntegrationTest.class)
-public class ClientePerfTest {
+public class ComprobarUsuarioPerfTest {
 
 	private Cliente c= new Cliente("alex","1234","alex@gmail.com","Alex","Anton","Mota","27/09/1999");
 ;
 	
 	
 	
-	static Logger logger = Logger.getLogger(ClientePerfTest.class.getName());
+	static Logger logger = Logger.getLogger(ComprobarUsuarioPerfTest.class.getName());
 
 	// If you use the EmptyReportModule, the report is not generated
 	//@Rule public ContiPerfRule rule = new ContiPerfRule(new EmptyReportModule());
 	
 	public static junit.framework.Test suite() {
-		 return new JUnit4TestAdapter(ClientePerfTest.class);
+		 return new JUnit4TestAdapter(ComprobarUsuarioPerfTest.class);
 	}
 	
 	@Rule
